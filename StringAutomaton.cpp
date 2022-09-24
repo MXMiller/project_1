83a77aaa193589ca120c57e2ded2bb7c09d10ba3
MXMiller <maxrmiller55@gmail.com>
2022-09-23 20:27:26 -0600

    commit

--- StringAutomaton.cpp
+++ StringAutomaton.cpp
@@ -17,16 +17,10 @@ void StringAutomaton::S1(const std::string& input) {
     if (input[index] == '\n') {
         this->newLines++;
         inputRead++;
-        //index++;
-        //S1(input);
+        index++;
+        S1(input);
     } else if (input[index] == '\'') {
-        if ((input[index + 1] != '\n')) {
-            inputRead++;
-            index++;
-            S1(input);
-        } else {
-            inputRead++;
-        }
+        inputRead++;
     } else {
         inputRead++;
         index++;
