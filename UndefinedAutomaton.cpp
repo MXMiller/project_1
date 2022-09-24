afe0dcbc1922dfb6666dcffe9fda77d919756ff7
MXMiller <maxrmiller55@gmail.com>
2022-09-23 19:20:16 -0600

    c

--- UndefinedAutomaton.cpp
+++ UndefinedAutomaton.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void UndefinedAutomaton::S0(const std::string& input) {
     if ((isalnum(input[index]) || !isalpha(input[index]))
-    & input[index] != ',' & input[index] != '.' & input[index] != '?'
-    & input[index] != '(' & input[index] != ')' & input[index] != ':'
-    & input[index] != '*' & input[index] != '+' & !isspace(input[index])) {
+    & ((input[index] != ',') & (input[index] != '.') & (input[index] != '?')
+    & (input[index] != '(') & (input[index] != ')') & (input[index] != ':')
+    & (input[index] != '*') & (input[index] != '+') & (!isspace(input[index])))) {
         inputRead++;
     }
     else {
