909939ea2b2f1fd7a1520433742068f635e8ada1
MXMiller <maxrmiller55@gmail.com>
2022-09-23 22:53:24 -0600

    commit

--- StringAutomaton.cpp
+++ StringAutomaton.cpp
@@ -20,18 +20,30 @@ void StringAutomaton::S1(const std::string& input) {
         index++;
         S1(input);
     }
-    /*
+
+    //INCLUDING THIS ELSE-IF MAKE 90-2 WORK, BUT BREAKS EVERYTHING ELSE
     else if ((input[index] == '\'') & (!isspace(input[index + 1]))) {
         inputRead++;
         index++;
-        S1(input);
+        S2(input);
     }
-    */
+
     else if ((input[index] == '\'')) {
         inputRead++;
     } else {
         inputRead++;
         index++;
         S1(input);
+    }
+}
+
+void StringAutomaton::S2(const std::string& input) {
+    if ((input[index] == '\'') || (input[index + 1] == '\'')) {
+        inputRead++;
+        index++;
+        S1(input);
+    }
+    else {
+        Serr();
     }
 }
